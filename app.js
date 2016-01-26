79d41a15e3418ebc1096f7172d718dab999a28be
Sopl’Wang <sopl.wang@gmail.com>
2016-01-27 07:45:58 +0800

    Remove template and template engine
    pure api project

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var Collider = require('./lib/collider');
@@ -12,18 +10,11 @@ var Collider = require('./lib/collider');
 // Application
 var app = express();
 
-// settings
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.set('websocket endpoints', {'/ws': require('./routes/ws')});
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // locals
 app.locals['collider'] = new Collider();
@@ -47,7 +38,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -58,7 +49,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
